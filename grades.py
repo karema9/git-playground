92167d97185565083b010de052eb90817b2e0f14
Cavin Karema <cavin.mulewa.karema@gmail.com>
2024-07-20 19:10:04 +0300

    added python program to assign grades

---
+++ grades.py
@@ -0,0 +1,11 @@
+grade = eval(input( 'Enter your score: '))
+if grade>=90:
+    print( 'A ')
+if grade>=80 and grade<90:
+    print( 'B ')
+if grade>=70 and grade<80:
+    print( 'C ')
+if grade>=60 and grade<70:
+    print( 'D ')
+if grade<60:
+    print( 'F ')
